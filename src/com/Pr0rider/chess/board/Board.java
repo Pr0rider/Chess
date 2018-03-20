7077cd406aa19899af252aee209c438f861e9768
Pr0rider <grzegorzgatkowski@gmail.com>
2018-03-20 22:09:41 +0100

    Version 0.7.1. Board builder.

--- src/com/Pr0rider/chess/board/Board.java
+++ src/com/Pr0rider/chess/board/Board.java
@@ -2,7 +2,17 @@ package com.Pr0rider.chess.board;
 
 public class Board {
 
+    private Board(Builder builder) {
+    }
+
     public Tile getTile (final int tileCoordinate){
         return null;
+    }
+
+    public static class Builder {
+
+        public Board build (){
+            return new Board (this);
+        }
     }
 }
